a953f383cb59b5d173a19a21fc14af61c7ecf7e6
ahalex73 <ahalex73@yahoo.com>
2021-04-23 18:01:53 +0000

    Final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var principal = 0;
 var rate = 10.25;
 var years = 1;
 var interest = 0;
-var year = 0;
+var year = 1;
 var amount = 0;
 
 function compute(){
@@ -12,7 +12,7 @@ function compute(){
     years = document.getElementById("years").value;
     year = new Date().getFullYear()+parseInt(years);
     interest = principal * years * rate /100;
-    amount = (interest + principal);
+    amount = interest + parseFloat(principal);
 
     if (principal > 0) {
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
