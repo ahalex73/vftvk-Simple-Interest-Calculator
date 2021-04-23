758b5f9eadcf25f4d22c0f0ae1afe4b6c0c9b79d
ahalex73 <ahalex73@yahoo.com>
2021-04-23 06:24:09 +0000

    hopefully prints principal

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ var rate_val = document.getElementById("rate_val").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-var amount = principal(1 + (rate_val * year));
-
 
 function compute(){
-    if (principal >= 0) {
-        document.getElementById("result").innerHTML="If you deposit "+",\<br\>at an interest rate of "+"%\<br\>You will receive an amount of "+",\<br\>in the year "+"\<br\>"
+    //if (principal >= 0) {
+        document.getElementById("result").innerHTML= principal
+        //document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     }
-    else {
-        alert("Enter a positive number")
+    //else {
+        //alert("Enter a positive number")
     }
 }
