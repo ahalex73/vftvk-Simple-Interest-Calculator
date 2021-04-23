3c499a56a4a56fb8d0b0ef53a4705ec9ce4782d8
ahalex73 <ahalex73@yahoo.com>
2021-04-23 06:31:25 +0000

    ok

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 
-function compute(){
+//function compute(){
     //if (principal >= 0) {
         //document.getElementById("result").innerHTML= principal
         //document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-    }
+    //}
     //else {
         //alert("Enter a positive number")
-    }
-}
+    //}
+//}
