effa86c7c6f1d0c72312e7a52cc4ebb526cd49e1
ahalex73 <ahalex73@yahoo.com>
2021-04-23 05:09:07 +0000

    ok

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
+var rate_val = document.getElementById("rate_val").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-
 
 function compute()
 {
