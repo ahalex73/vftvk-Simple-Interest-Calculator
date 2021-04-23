e73e432de1f6a518f572d7dcfb05efbd1cf23823
ahalex73 <ahalex73@yahoo.com>
2021-04-23 17:17:18 +0000

    ok

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-//var principal = document.getElementById("principal").value;
-//var rate = document.getElementById("rate").value;
-//var rate_val = document.getElementById("rate_val").value;
-//var years = document.getElementById("years").value;
-//var interest = principal * years * rate /100;
-//var year = new Date().getFullYear()+parseInt(years);
-//var amount = interest + principal
+var principal = 0;
+var rate = document.getElementById("rate").value;
+var rate_val = document.getElementById("rate_val").value;
+var years = document.getElementById("years").value;
+var interest = 0;
+var year = new Date().getFullYear()+parseInt(years);
+var amount = 0;
 
 function compute(){
     // checking if principal is a negative number or 0, if not continue, if so alert
+    principal = document.getElementById("principal").value;
+    interest = principal * years * rate /100;
+    amount = interest + principal;
+
     if (principal > 0) {
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
     }
