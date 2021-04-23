3ed739e0c49a74d9cc57a6ea98350d1fbfa7ebe9
ahalex73 <ahalex73@yahoo.com>
2021-04-23 06:04:44 +0000

    what.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var amount = principal(1 + (rate_val * year));
 
 
 function compute(){
-    if (principal >= 0) {
+    if (principal <= 0) {
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
     }
     else {
