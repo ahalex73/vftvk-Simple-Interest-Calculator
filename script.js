6941f996c101abfb5cd54430ef6af40b1af00a96
ahalex73 <ahalex73@yahoo.com>
2021-04-23 17:58:00 +0000

    workingButBRoekn

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var principal = 0;
 var rate = 10.25;
-var rate_val = 10.25;
 var years = 1;
 var interest = 0;
 var year = 0;
@@ -10,14 +9,13 @@ function compute(){
     // checking if principal is a negative number or 0, if not continue, if so alert
     principal = document.getElementById("principal").value;
     rate = document.getElementById("rate").value;
-    rate_val = document.getElementById("rate_val").value;
     years = document.getElementById("years").value;
     year = new Date().getFullYear()+parseInt(years);
     interest = principal * years * rate /100;
     amount = (interest + principal);
 
     if (principal > 0) {
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     }
     else {
         alert("Enter a positive number")
