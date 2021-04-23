3555f2ab4b3625d5f2bb210368bc6183aa345136
ahalex73 <ahalex73@yahoo.com>
2021-04-23 17:27:53 +0000

    ok

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var principal = 0;
-var rate = document.getElementById("rate").value;
-var rate_val = document.getElementById("rate_val").value;
-var years = document.getElementById("years").value;
+var rate = 10.25;
+var rate_val = 10.25;
+var years = 1;
 var interest = 0;
-var year = new Date().getFullYear()+parseInt(years);
+var year = 0;
 var amount = 0;
 
 function compute(){
     // checking if principal is a negative number or 0, if not continue, if so alert
     principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    rate_val = document.getElementById("rate_val").value;
+    years = document.getElementById("years").value;
+    year = new Date().getFullYear()+parseInt(years);
     interest = principal * years * rate /100;
     amount = interest + principal;
 
